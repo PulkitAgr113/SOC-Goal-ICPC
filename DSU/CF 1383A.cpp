90e3d9715662f540270e0a8d89d3d6c27bbae76e
Pulkit Agarwal <82202690+PulkitAgr113@users.noreply.github.com>
2021-06-11 17:36:49 +0530

    String DSU

---
+++ DSU/CF 1383A.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f(i,x,n) for (int i=x;i<n;i++)
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+
+typedef long long int ll ;
+typedef vector<int> vi ;
+typedef queue<int> qi ;
+typedef vector<vector<int>> vvi ;
+typedef vector<vector<ll>> vvll ;
+typedef vector<vector<pair<int,int>>> vvp ;
+typedef vector<bool> vb ;
+typedef vector<double> vd ;
+typedef vector<vector<char>> vvc ;
+typedef vector<char> vc ;
+typedef pair<int,int> pii ;
+typedef vector<pair<int,int>> vpi ;
+typedef vector<pair<ll,ll>> vpll ;
+typedef pair<ll,ll> pll ;
+typedef vector<ll> vll ;
+typedef stack<int> si ;
+typedef map<int,int> mii ;
+typedef set<int> seti ;
+typedef double db ;
+
+int mod1 = 998244353 ;
+int mod2 = 1000000007 ;
+int Max_M = 20 ;
+
+vi par(Max_M),sz(Max_M);
+
+void make_set(int v) {
+    par[v]=v;
+    sz[v]=1;
+}
+
+int find_set(int v) {
+    if(par[v]==v) return v;
+    return par[v]=find_set(par[v]);
+}
+
+void union_sets(int a, int b) {
+    a=find_set(a);
+    b=find_set(b);
+    if(a!=b) {
+        if(sz[a]<sz[b]) swap(a,b);
+        par[b]=a;
+        sz[a]+=sz[b];
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false) ; cin.tie(NULL) ; cout.tie(NULL) ;
+    int t,n;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        string a,b;
+        cin>>a>>b;
+        f(i,0,20) make_set(i);
+        bool ans=true;
+        int moves=0;
+        f(i,0,n) {
+            if(b[i]-a[i]<0) {
+                ans=false;
+                break;
+            }
+            else if(b[i]!=a[i]) union_sets(a[i]-'a',b[i]-'a');
+        }
+        f(i,0,20) {
+            if(find_set(i)==i) moves+=sz[i]-1;
+        }
+        if(ans) cout<<moves<<"\n";
+        else cout<<"-1\n";
+    }
+    return 0;
+}
